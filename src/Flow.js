67e43342fb2d4b5382793dc93f85416d8563e533
Sebastian Sastre <sebastian@flowingconcept.com>
2014-08-09 19:36:57 -0300

    wip(scaffolding): basics in place

--- src/Flow.js
+++ src/Flow.js
@@ -3,7 +3,157 @@ var smalltalk=$boot.vm,nil=$boot.nil,_st=$boot.asReceiver,globals=$boot.globals;
 smalltalk.addPackage('Flow');
 smalltalk.packages["Flow"].transport = {"type":"amd","amdNamespace":"app"};
 
+smalltalk.addClass('AbsrtactGenerator', globals.Object, [], 'Flow');
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "scaffold:",
+protocol: 'actions',
+fn: function (anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1=self._subclassResponsibility();
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"scaffold:",{anObject:anObject},globals.AbsrtactGenerator.klass)})},
+args: ["anObject"],
+source: "scaffold: anObject\x0a\x09\x22Scaffold what's relevnt to this generator\x22\x0a\x09\x0a\x09^ self subclassResponsibility",
+messageSends: ["subclassResponsibility"],
+referencedClasses: []
+}),
+globals.AbsrtactGenerator.klass);
+
+
+smalltalk.addClass('CRUDGenerator', globals.AbsrtactGenerator, [], 'Flow');
+
+
+smalltalk.addClass('MaplessGenerator', globals.AbsrtactGenerator, [], 'Flow');
+
+
+smalltalk.addClass('ModelGenerator', globals.AbsrtactGenerator, [], 'Flow');
+
+
 smalltalk.addClass('Flow', globals.Object, [], 'Flow');
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "generate",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+return (3);
+},
+args: [],
+source: "generate\x0a\x0a\x09^ 3",
+messageSends: [],
+referencedClasses: []
+}),
+globals.Flow.klass);
+
+
+smalltalk.addClass('Generator', globals.Object, [], 'Flow');
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "crud:",
+protocol: 'actions',
+fn: function (aMaplessClassOrName){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1=_st(self._crudGenerator())._scaffold_(aMaplessClassOrName);
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"crud:",{aMaplessClassOrName:aMaplessClassOrName},globals.Generator.klass)})},
+args: ["aMaplessClassOrName"],
+source: "crud: aMaplessClassOrName\x0a\x0a\x09^ self crudGenerator scaffold: aMaplessClassOrName",
+messageSends: ["scaffold:", "crudGenerator"],
+referencedClasses: []
+}),
+globals.Generator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "crudGenerator",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+function $CRUDGenerator(){return globals.CRUDGenerator||(typeof CRUDGenerator=="undefined"?nil:CRUDGenerator)}
+return $CRUDGenerator();
+},
+args: [],
+source: "crudGenerator\x0a\x0a\x09^ CRUDGenerator ",
+messageSends: [],
+referencedClasses: ["CRUDGenerator"]
+}),
+globals.Generator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "mapless:",
+protocol: 'actions',
+fn: function (aMaplessName){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1=_st(self._maplessGenerator())._scaffold_(aMaplessName);
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"mapless:",{aMaplessName:aMaplessName},globals.Generator.klass)})},
+args: ["aMaplessName"],
+source: "mapless: aMaplessName\x0a\x0a\x09^ self maplessGenerator scaffold: aMaplessName",
+messageSends: ["scaffold:", "maplessGenerator"],
+referencedClasses: []
+}),
+globals.Generator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "maplessGenerator",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+function $MaplessGenerator(){return globals.MaplessGenerator||(typeof MaplessGenerator=="undefined"?nil:MaplessGenerator)}
+return $MaplessGenerator();
+},
+args: [],
+source: "maplessGenerator\x0a\x0a\x09^ MaplessGenerator ",
+messageSends: [],
+referencedClasses: ["MaplessGenerator"]
+}),
+globals.Generator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "model:",
+protocol: 'actions',
+fn: function (aModelName){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+var $1;
+$1=_st(self._modelGenerator())._scaffold_(aModelName);
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"model:",{aModelName:aModelName},globals.Generator.klass)})},
+args: ["aModelName"],
+source: "model: aModelName\x0a\x0a\x09^ self modelGenerator scaffold: aModelName",
+messageSends: ["scaffold:", "modelGenerator"],
+referencedClasses: []
+}),
+globals.Generator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "modelGenerator",
+protocol: 'accessing',
+fn: function (){
+var self=this;
+function $ModelGenerator(){return globals.ModelGenerator||(typeof ModelGenerator=="undefined"?nil:ModelGenerator)}
+return $ModelGenerator();
+},
+args: [],
+source: "modelGenerator\x0a\x0a\x09^ ModelGenerator ",
+messageSends: [],
+referencedClasses: ["ModelGenerator"]
+}),
+globals.Generator.klass);
 
 smalltalk.addMethod(
 smalltalk.method({
